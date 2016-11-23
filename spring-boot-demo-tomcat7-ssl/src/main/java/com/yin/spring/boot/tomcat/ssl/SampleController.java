da378b00711c33adaf98312f47bbde09fd714ff6
yinxunzhi <yinxunzhi@che001.com>
2016-11-23 15:10:01 +0800

    spring boot tomcat 7  ssl使用

---
+++ spring-boot-demo-tomcat7-ssl/src/main/java/com/yin/spring/boot/tomcat/ssl/SampleController.java
@@ -0,0 +1,18 @@
+package com.yin.spring.boot.tomcat.ssl;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Eason on 2016/11/23.
+ */
+@Controller
+public class SampleController {
+
+    @GetMapping
+    @ResponseBody
+    public String hello(){
+        return "hello world";
+    }
+}
---
+++ spring-boot-demo-tomcat7-ssl/src/main/java/com/yin/spring/boot/tomcat/ssl/SampleTomcatSslApplication.java
@@ -0,0 +1,15 @@
+package com.yin.spring.boot.tomcat.ssl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Eason on 2016/11/23.
+ */
+@SpringBootApplication
+public class SampleTomcatSslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleTomcatSslApplication.class,args);
+    }
+}
